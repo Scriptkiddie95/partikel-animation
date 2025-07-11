7ecc31e01577c5690d1b829cdaf2cfd161699785
Brian <105023855+Scriptkiddie95@users.noreply.github.com>
2025-07-11 23:35:17 +0200

    chore: specify root index for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Explicitly use the root index.html file as the entry point
+  root: '.',
   plugins: [react()],
 })
